f876556a63941e1fc1aa6cbf9629531a898d86c3
Tivah <tivakudzezishiri@gmail.com>
2023-08-15 07:31:28 -0700

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- Insert values
--- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- Inserts a new row in a table
+-- Query to insert a new row in the table first_table
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
