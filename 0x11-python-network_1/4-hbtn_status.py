6ce6d961b6c8f3ed0bd281f338f9179e1b565c7f
Tivah <tivakudzezishiri@gmail.com>
2023-09-29 14:24:42 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" use requests for make requests
 """
+Python script that fetches an URL with requests package
+"""
+import requests
+
 
 if __name__ == "__main__":
-    import requests as pat
-    r = pat.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type:", type(r.text))
-    print("\t- content:", r.text)
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
