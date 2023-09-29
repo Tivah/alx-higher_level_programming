ac28a1fb4d4d10d744fb8ac76787273e5556a658
Tivah <tivakudzezishiri@gmail.com>
2023-09-29 14:22:02 -0700

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-import requests
-import sys
+"""
+Sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response."""
 
-try:
-    my_data = {'email': sys.argv[2]}
-    res = requests.post(sys.argv[1], my_data)
+
+if __name__ == '__main__':
+    from sys import argv
+    from requests import post
+
+    url = argv[1]
+    email = argv[2]
+    res = post(url, {'email': email})
     print(res.text)
-except BaseException:
-    pass
